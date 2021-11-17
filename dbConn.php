f0d71473c0b69e012f541d4f583a0b14277e0392
Alyssa <78221325+lawwalyy@users.noreply.github.com>
2021-11-17 16:51:15 +0800

    Update dbConn.php

--- dbConn.php
+++ dbConn.php
@@ -2,9 +2,9 @@
 $hostName = "localhost";
 $userName = "root";
 $password = "";
-// Create connection
+
 $conn = mysqli_connect('localhost', 'root', '');
-// Check connection
+
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
